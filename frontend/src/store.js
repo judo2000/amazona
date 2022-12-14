d738a1e4802c8c9ffbc236d298d309f14fce5f42
Stephen Moore <judo2000@comcast.net>
2022-12-14 14:15:47 -0700

    Complete cart screen and save cart items to localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,10 +4,11 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
   },
 };
-
 function reducer(state, action) {
   switch (action.type) {
     case 'CART_ADD_ITEM':
@@ -21,12 +22,20 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
-
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
     default:
       return state;
   }
 }
+
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
